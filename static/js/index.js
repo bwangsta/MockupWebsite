50486aae85eb927d45fa3466181324bf70589c2c
Brian Wang <60533686+bwangsta@users.noreply.github.com>
2020-08-15 19:33:08 -0700

    Initial Deploy

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,4 @@
 var clicks = 0;
-// var blueClicks = 0;
-// var greenClicks = 0;
-// var yellowClicks = 0;
 
 $("#redBtn").on("click", function() {
     if($("#redDye").css("display") === "none") {
@@ -10,16 +7,6 @@ $("#redBtn").on("click", function() {
                 $("#redDye").fadeIn();
             });
         }
-        // else if($("#greenDye").css("display") != "none") {
-        //     $("#greenDye").fadeOut(function() {
-        //         $("#redDye").fadeIn();
-        //     });
-        // }
-        // else if($("#yellowDye").css("display") != "none") {
-        //     $("#yellowDye").fadeOut(function() {
-        //         $("#redDye").fadeIn();
-        //     })
-        // }
         else {
             $("#redDye").fadeIn();
         }
@@ -38,16 +25,6 @@ $("#blueBtn").on("click", function() {
                 $("#blueDye").fadeIn();
             });
         }
-        // else if($("#greenDye").css("display") != "none") {
-        //     $("#greenDye").fadeOut(function() {
-        //         $("#blueDye").fadeIn();
-        //     });
-        // }
-        // else if($("#yellowDye").css("display") != "none") {
-        //     $("#yellowDye").fadeOut(function() {
-        //         $("#blueDye").fadeIn();
-        //     })
-        // }
         else {
             $("#blueDye").fadeIn();
         }
@@ -58,59 +35,3 @@ $("#blueBtn").on("click", function() {
         $("#blueDye").fadeOut();
     }
 });
-
-// $("#greenBtn").on("click", function() {
-//     if($("#greenDye").css("display") === "none") {
-//         if($("#redDye").css("display") != "none") {
-//             $("#redDye").fadeOut(function() {
-//                 $("#greenDye").fadeIn();
-//             });
-//         }
-//         else if($("#blueDye").css("display") != "none") {
-//             $("#blueDye").fadeOut(function() {
-//                 $("#greenDye").fadeIn();
-//             });
-//         }
-//         else if($("#yellowDye").css("display") != "none") {
-//             $("#yellowDye").fadeOut(function() {
-//                 $("#greenDye").fadeIn();
-//             })
-//         }
-//         else {
-//             $("#greenDye").fadeIn();
-//         }
-//         greenClicks++;
-//         $(".greenDisplay").text(greenClicks);
-//     }
-//     else {
-//         $("#greenDye").fadeOut();
-//     }
-// });
-
-// $("#yellowBtn").on("click", function() {
-//     if($("#yellowDye").css("display") === "none") {
-//         if($("#redDye").css("display") != "none") {
-//             $("#redDye").fadeOut(function() {
-//                 $("#yellowDye").fadeIn();
-//             });
-//         }
-//         else if($("#blueDye").css("display") != "none") {
-//             $("#blueDye").fadeOut(function() {
-//                 $("#yellowDye").fadeIn();
-//             });
-//         }
-//         else if($("#greenDye").css("display") != "none") {
-//             $("#greenDye").fadeOut(function() {
-//                 $("#yellowDye").fadeIn();
-//             })
-//         }
-//         else {
-//             $("#yellowDye").fadeIn();
-//         }
-//         yellowClicks++;
-//         $(".yellowDisplay").text(yellowClicks);
-//     }
-//     else {
-//         $("#yellowDye").fadeOut();
-//     }
-// });
