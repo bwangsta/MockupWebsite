a8f4f659869883c46758f83ed0bf0cf3feac0a22
Brian Wang <60533686+bwangsta@users.noreply.github.com>
2020-09-03 15:32:59 -0700

    Converted to package

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html", title="About")
+from mockup import app
 
 if __name__ == "__main__":
     app.run(debug=True)
