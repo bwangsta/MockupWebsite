33f1b4c80a4c16716a2fbce4e102f9f225f0d639
Brian Wang <bwbrianwang1018@gmail.com>
2023-08-04 20:42:32 -0700

    Remove annoying modals

--- mockup/static/js/index.js
+++ mockup/static/js/index.js
@@ -1,49 +1,41 @@
-var clicks = 0;
+var clicks = 0
 
-$(window).on('load',function(){
-    $('#newsletter').modal('show');
-});
+$("#redBtn").on("click", function () {
+  if ($("#redDye").css("display") === "none") {
+    if ($("#blueDye").css("display") != "none") {
+      $("#blueDye").fadeOut(function () {
+        $("#redDye").fadeIn(function () {
+          clicks++
+          $(".countDisplay").text(clicks)
+        })
+      })
+    } else {
+      $("#redDye").fadeIn(function () {
+        clicks++
+        $(".countDisplay").text(clicks)
+      })
+    }
+  } else {
+    $("#redDye").fadeOut()
+  }
+})
 
-$("#redBtn").on("click", function() {
-    if($("#redDye").css("display") === "none") {
-        if($("#blueDye").css("display") != "none") {
-            $("#blueDye").fadeOut(function() {
-                $("#redDye").fadeIn(function() {
-                    clicks++;
-                    $(".countDisplay").text(clicks);
-                });
-            });
-        }
-        else {
-            $("#redDye").fadeIn(function() {
-                clicks++;
-                $(".countDisplay").text(clicks);
-            });
-        }
+$("#blueBtn").on("click", function () {
+  if ($("#blueDye").css("display") === "none") {
+    if ($("#redDye").css("display") != "none") {
+      $("#redDye").fadeOut(function () {
+        $("#blueDye").fadeIn(function () {
+          clicks++
+          $(".countDisplay").text(clicks)
+        })
+      })
+    } else {
+      $("#blueDye").fadeIn(function () {
+        clicks++
+        $(".countDisplay").text(clicks)
+      })
     }
-    else {
-        $("#redDye").fadeOut();
-    }
-});
-
-$("#blueBtn").on("click", function() {
-    if($("#blueDye").css("display") === "none") {
-        if($("#redDye").css("display") != "none") {
-            $("#redDye").fadeOut(function() {
-                $("#blueDye").fadeIn(function() {
-                    clicks++;
-                    $(".countDisplay").text(clicks);
-                });
-            });
-        }
-        else {
-            $("#blueDye").fadeIn(function() {
-                clicks++;
-                $(".countDisplay").text(clicks);
-            });
-        }
-    }
-    else {
-        $("#blueDye").fadeOut();
-    }
-});
+  } else {
+    $("#blueDye").fadeOut()
+  }
+})
